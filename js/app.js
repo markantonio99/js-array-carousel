379d5993a322ee57fcc3eb046bfc274951210a20
“markantonio99” <markomakkia@gmail.com>
2023-02-06 19:02:38 +0100

    salv 3

--- js/app.js
+++ js/app.js
@@ -5,16 +5,19 @@ console.log(carouselEl)
 const btnRightElement = document.getElementById('btn_r')
 console.log(btnRightElement)
 
+let functionActiv = 0
+
 btnRightElement.addEventListener( 'click', function() {
     console.log('next right')
     
-let firstSlide = carouselEl[0]
+let firstSlide = carouselEl[functionActiv]
 console.log(firstSlide)
-firstSlide.classList.remove('mk_display_active')
+firstSlide.classList.toggle('mk_display_active')
 
+functionActiv +=1 
 
-let secondSlide = carouselEl[1]
+let secondSlide = carouselEl[functionActiv]
 console.log(secondSlide)
-secondSlide.classList.add('mk_display_active')
+secondSlide.classList.toggle('mk_display_active')
 
 })
